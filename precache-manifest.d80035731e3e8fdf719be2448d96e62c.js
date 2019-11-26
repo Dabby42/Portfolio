6a0809aaf326972c1c3c6e956f6614f7533bd532
unknown <nwafordabere@gmail.com>
2019-11-26 01:01:28 +0100

    Updates

---
+++ precache-manifest.d80035731e3e8fdf719be2448d96e62c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30d477a21daf720d567ebcbf63fcf9f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c27ad491b3793353ee4e",
+    "url": "/Portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "02ff375df5e5a0b0ae2f",
+    "url": "/Portfolio/static/js/2.7d12e2e4.chunk.js"
+  },
+  {
+    "revision": "c27ad491b3793353ee4e",
+    "url": "/Portfolio/static/js/main.5c4edc7e.chunk.js"
+  },
+  {
+    "revision": "3250d1d6e6c9225ef482",
+    "url": "/Portfolio/static/js/runtime~main.161fe929.js"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/Portfolio/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/Portfolio/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/Portfolio/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/Portfolio/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/Portfolio/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/Portfolio/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "e25053c453155a9067f3cdc1bdbdd2a1",
+    "url": "/Portfolio/static/media/1.e25053c4.jpg"
+  },
+  {
+    "revision": "7006d6963a5fc645c84ff0e3e36e6a79",
+    "url": "/Portfolio/static/media/2.7006d696.jpg"
+  },
+  {
+    "revision": "838cdb868e76074f31c9a7776f9b92e5",
+    "url": "/Portfolio/static/media/3.838cdb86.jpg"
+  },
+  {
+    "revision": "0133942f19f01bc32c2030794677f760",
+    "url": "/Portfolio/static/media/about.0133942f.jpg"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/Portfolio/static/media/header-bg.e2d2a51c.jpg"
+  }
+]);
